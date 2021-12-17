012f928f70582d0fa7a1d984c85d8caa68105ebc
Nultake <81858452+Nultake@users.noreply.github.com>
2021-12-17 23:37:19 +0300

    Add files via upload

---
+++ OnClick/OnClick/AuthorizedAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnClick
+{
+    internal class AuthorizedAccount : Account
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public static List<AuthorizedAccount> authorizedUsers = new List<AuthorizedAccount>();
+
+        public AuthorizedAccount(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+    }
+}
---
+++ OnClick/OnClick/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnClick
+{
+    internal class Program
+    {
+        public static Account account;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
